14c8c7dabaa22a4faf64b053119552549a5bf98b
charles.yu <charles_yxh@163.com>
2020-04-13 14:25:09 +0800

    feat: 修改上传组件

--- src/views/demo/upload.vue
+++ src/views/demo/upload.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <image-upload :files="files" @getFiles="getFiles" />
+    <normal-upload :files="files" @getFiles="getFiles" />
   </div>
 </template>
 
 <script>
 import ImageUpload from '@/components/Upload/Image'
+import NormalUpload from '@/components/Upload/Normal'
+
 export default {
   name: 'Upload',
   components: {
-    ImageUpload
+    ImageUpload,
+    NormalUpload
   },
   data() {
     return {
