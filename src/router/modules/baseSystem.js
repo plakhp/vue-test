d89e5bbff4834a2bb11586bcbcb36428a9b6c154
夏传露 <clxia@ipzoe.com>
2020-05-13 10:21:45 +0800

    隐藏菜单

--- src/router/modules/baseSystem.js
+++ src/router/modules/baseSystem.js
@@ -12,15 +12,15 @@ const router = {
         import ('@/views/baseSystem/shop_class'),
     meta: { title: '商户分类',  affix: true, roles: ['shop_category_manager'], activeMenu: '/baseSystem/shop_class' },
     children: []
+  },
+  {
+    path: 'word',
+    name: '热门关键词设置',
+    component: () =>
+        import ('@/views/baseSystem/word'),
+    meta: { title: '热门关键词设置',  affix: true, roles: ['word_manager'], activeMenu: '/baseSystem/word' },
+    children: []
   }
-  // {
-  //   path: 'word',
-  //   name: '热门关键词设置',
-  //   component: () =>
-  //       import ('@/views/baseSystem/word'),
-  //   meta: { title: '热门关键词设置',  affix: true, roles: ['word_manager'], activeMenu: '/baseSystem/word' },
-  //   children: []
-  // }
 
   ]
 }
