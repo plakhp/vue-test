689d08108bbe78ee18d22a9a8cec566711701adf
夏传露 <clxia@ipzoe.com>
2020-05-13 14:49:58 +0800

    权限修改

--- src/router/modules/system.js
+++ src/router/modules/system.js
@@ -6,13 +6,13 @@ const router = {
   path: '/system',
   component: Layout,
   name: '系统管理',
-  meta: { title: '系统管理',  affix: true, roles: ['system_manager'] },
+  meta: { title: '系统管理', affix: true, roles: ['oauth_manager'] },
   children: [
     {
       path: 'account',
       name: '账号管理',
       component: () => import('@/views/system/account/index'),
-      meta: { title: '账号管理',  affix: true, roles: ['account_manager'], activeMenu: '/system/account' },
+      meta: { title: '账号管理', affix: true, roles: ['account_manager'], activeMenu: '/system/account' },
       children: []
     },
     {
@@ -26,9 +26,10 @@ const router = {
     //   path: 'menu',
     //   name: '菜单管理',
     //   component: () => import('@/views/system/menu/index'),
-    //   meta: { title: '菜单管理',  affix: true, roles: ['menu_manager'], activeMenu: '/system/menu' },
+    //   meta: { title: '菜单管理', affix: true, roles: ['menu_manager'], activeMenu: '/system/menu' },
     //   children: []
     // }
+
   ]
 }
 
